4dc4879434a37033cc1ba9b2d087b1c8a093be1f
Shashank Satheesh <shashank_satheesh@thbs.india.com>
2022-12-27 11:23:02 +0530

    AntDesignBreadcrumbs.js & antDesignBreadcrumbs.css fixed backgroundcolor&last bredacrum highlighted now it is fine

--- src/components/ant-design-breadcrumbs/AntDesignBreadcrumbs.js
+++ src/components/ant-design-breadcrumbs/AntDesignBreadcrumbs.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Breadcrumb, Layout } from "antd";
 
+import "./antDesignBreadcrumbs.css"
 const { Content } = Layout;
 const { Item } = Breadcrumb;
-
+ 
 //! like below json array of objects should be passed as props to use AntDesignBreadcrumb
 //! title- value is like home, product what ever you want to display in BreadCrumbs
 //! navigationPath- value should be a path of particular BreadCrumbs or navigationPath should be like '' to disable
