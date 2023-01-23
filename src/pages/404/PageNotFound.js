10b87a35b868cf00e8d9f7dc3671680240c3ec87
Shalini Muthuraman <shalini_muthuraman@thbs.india.com>
2023-01-23 19:48:39 +0530

    authorization validation done

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Layout, Typography } from "antd";
 import { keycloakData } from "../../urlPages/keycloak";
 
-
+const { Content } = Layout;
+const { Title } = Typography;
 const keycloakUrl = keycloakData.url
-const PageNotFound = () => {
-  // return <h1>Page not found</h1>;
-  return <h1>Please login to access this page <a href={keycloakUrl}>Login</a></h1>
-};
+function PageNotFound() {
+  return (
+    <Layout className="h-[500px]">
+    <Content className="mt-[64px] grid justify-items-center align-items-center h-full">
+      <Title level={4}>
+        The Page you are trying to access is{" "}
+        <span className="text-red-600 underline">Not Available</span>, 
+        Please login to access this page <a href={keycloakUrl}>Login </a>
+      </Title>
+    </Content>
+    </Layout>
+  );
+}
 
 export default PageNotFound;
+
