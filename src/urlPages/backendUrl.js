e4e943cc5593218523c01b681d665c88e34a49a8
Sushil Sarraf <sushil_sarraf@thbs.india.com>
2023-02-09 14:03:02 +0530

    Commented config files under urlPages as they are moved to env file

--- src/urlPages/backendUrl.js
+++ src/urlPages/backendUrl.js
@@ -1,6 +1,6 @@
-export const backendUrl = {
-  'isLoggedInURL': 'http://localhost:5000/authenticate/rest/v2/isloggedin',
-  'getPermissionsUrl': 'http://localhost:5000/authenticate/rest/v2/get_permission',
-  'logoutUrl': 'http://localhost:5000/authenticate/rest/v2/logout',
-  'getAccessTokenUrl': 'http://localhost:5000/authenticate/rest/v2/accesstoken'
-}
+// export const backendUrl = {
+//   'isLoggedInURL': 'http://localhost:5000/authenticate/rest/v2/isloggedin',
+//   'getPermissionsUrl': 'http://localhost:5000/authenticate/rest/v2/get_permission',
+//   'logoutUrl': 'http://localhost:5000/authenticate/rest/v2/logout',
+//   'getAccessTokenUrl': 'http://localhost:5000/authenticate/rest/v2/accesstoken'
+// }
