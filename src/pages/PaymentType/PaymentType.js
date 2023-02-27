03b6c9f695017c19e570b364c07eb7e45ac24252
Sandhya Allu <sandhya_allu@thbs.india.com>
2023-02-27 18:04:30 +0530

    added dynamic table in payment types and given static data in that table

--- src/pages/PaymentType/PaymentType.js
+++ src/pages/PaymentType/PaymentType.js
@@ -1,7 +1,104 @@
 import React from "react";
+import { Layout, Typography } from "antd";
+
+import DynamicTable from "../../components/DynamicTable/DynamicTable";
+import AntDesignBreadcrumbs from "../../components/ant-design-breadcrumbs/AntDesignBreadcrumbs";
+
+const { Content } = Layout;
+const { Title } = Typography;
+
+const paymentTypesData = [
+  {
+    count: 3,
+    page_number: 1,
+    page_limit: 20,
+    data: [
+      {
+        id: 1,
+        payment_type: "COD",
+        is_payment_gateway: false,
+      },
+      {
+        id: 2,
+        payment_type: "Online Payment",
+        is_payment_gateway: true,
+      },
+      {
+        id: 3,
+        payment_type: "Purchase Order",
+        is_payment_gateway: true,
+      },
+    ],
+  },
+];
+
+const paymentTypeColumns = [
+  {
+    title: "Payment Type",
+    dataIndex: "paymenttype",
+    key: "paymenttype",
+    width: "40%",
+    render: (text, record) => {
+      return <>{record.name}</>;
+    },
+  },
+];
+
+const tempArray = [];
+paymentTypesData[0] &&
+  paymentTypesData[0].data.map((element, index) => {
+    var tempId = element.id;
+    var paymentTypeName = element.payment_type;
+    tempArray.push({
+      key: index,
+      name: paymentTypeName,
+      id: tempId,
+    });
+  });
+console.log("first", tempArray);
+
+const tablePropsData = {
+  table_header: paymentTypeColumns,
+  table_content: tempArray,
+  pagenationSettings: false,
+  search_settings: {
+    is_enabled: false,
+    search_title: "Search by name",
+    search_data: ["name"],
+  },
+  filter_settings: {
+    is_enabled: false,
+    filter_title: "Filter's",
+    filter_data: [],
+  },
+  sorting_settings: {
+    is_enabled: false,
+    sorting_title: "Sorting by",
+    sorting_data: [],
+  },
+};
 
 const PaymentType = () => {
-  return <div>PaymentType</div>;
+  return (
+    <Content>
+      <Content className="mb-1">
+        <AntDesignBreadcrumbs
+          data={[
+            { title: "Home", navigationPath: "/", displayOrder: 1 },
+            { title: "Payment Types", navigationPath: "", displayOrder: 2 },
+          ]}
+        />
+      </Content>
+      <Content className="mt-3 mb-3">
+        <Title level={3} className="!font-normal">
+          Payment Types
+        </Title>
+      </Content>
+      <Content>
+        <DynamicTable tableComponentData={tablePropsData} />
+      </Content>
+    </Content>
+  );
 };
 
 export default PaymentType;
