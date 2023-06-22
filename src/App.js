e028a8213cc204e7cc9f82355586f86424ccdfbc
Venkata Merapureddy <venkata_merapureddy@thbs.india.com>
2023-06-23 00:35:04 +0530

    Worked on the session related

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, useState } from "react";
 import { Container } from "reactstrap";
+import { Layout, Spin } from "antd";
 import {
   BrowserRouter as Router,
   Route,
@@ -31,36 +32,70 @@ import OnlinePaymentConnector from "./pages/OnlinePaymentConnector/OnlinePayment
 import StoreSettings from "./pages/StoreSetting/StoreSettings";
 import Preview from "./pages/StoreSetting/Preview";
 import UserProfile from "./pages/StoreUsers/UserProfile";
+import { LoadingOutlined } from "@ant-design/icons";
+
+import { useAuth } from "react-oidc-context";
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
-const auth = process.env.REACT_APP_AUTH;
+// const authFromEnv = process.env.REACT_APP_AUTH;
 
+const { Content } = Layout;
 const App = () => {
+  const auth = useAuth();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   let keycloakData = false;
   useFavicon();
   let isLoggedInFromSession;
 
-  if (auth === "true") {
-    axios.defaults.headers.common["Authorization"] =
-      sessionStorage.getItem("access_token");
-    isLoggedInFromSession = sessionStorage.getItem("is_loggedIn");
-    if (sessionStorage.getItem("keycloakData")) {
-      keycloakData = true;
-    }
-  } else {
-    isLoggedInFromSession = false;
+  // if (authFromEnv === "true") {
+  //   axios.defaults.headers.common["Authorization"] =
+  //     sessionStorage.getItem("access_token");
+  //   isLoggedInFromSession = sessionStorage.getItem("is_loggedIn");
+  //   if (sessionStorage.getItem("keycloakData")) {
+  //     keycloakData = true;
+  //   }
+  // } else {
+  //   isLoggedInFromSession = false;
+  // }
+
+  const antIcon = (
+    <LoadingOutlined
+      style={{
+        fontSize: 54,
+      }}
+      spin
+    />
+  );
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
   }
 
-  return (
-    <Suspense fallback={<LoadingMarkup />}>
-      <Router>
-        <ToastContainer />
-        <Header2 />
-        <Container fluid className="p-0 bg-[#F4F4F4] text-[#393939]">
-          {auth === "true" && (
+  if (auth.isLoading) {
+    return (
+      <Layout className="h-[500px]">
+        <Content className="grid justify-items-center align-items-center h-full">
+          <Spin indicator={antIcon} />
+        </Content>
+      </Layout>
+    );
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+  if (auth.isAuthenticated) {
+    return (
+      <Suspense fallback={<LoadingMarkup />}>
+        <Router>
+          <ToastContainer />
+          <Header2 />
+          <Container fluid className="p-0 bg-[#F4F4F4] text-[#393939]">
             <Routes>
-              <Route
+              {/* <Route
                 exact
                 path="*"
                 element={
@@ -70,114 +105,183 @@ const App = () => {
                     <Navigate to="/user-verification" />
                   )
                 }
-              />
-              <Route
-                path="/"
-                element={
-                  <Home setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
-                }
-              />
+              /> */}
+              <Route path="/" element={<Home />} />
               {/* <Route path="/home" element={<Navigate to={"/"} />} /> */}
-              <Route path="/user-verification" element={<PageNotFound />} />
-              <Route path="/signin" element={<Signin />} />
-              {keycloakData ? (
-                <Route path="/dashboard" element={<SidebarNew color="light" />}>
-                  <Route
-                    path=""
-                    element={
-                      <Dashboard
-                        setIsLoggedIn={setIsLoggedIn}
-                        isLoggedIn={isLoggedIn}
-                      />
-                    }
-                  />
-                  {isLoggedInFromSession ? (
-                    <>
-                      <Route path="language" element={<Language />} />
-                      <Route
-                        path="language/edit_language"
-                        element={<EditLanguage />}
-                      />
-                      <Route
-                        path="language/add_language"
-                        element={<AddLanguage />}
-                      />
-                      <Route path="store" element={<Store />} />
-                      <Route
-                        path="store/storesetting"
-                        element={<StoreSettings />}
-                      />
-                      <Route path="preview" element={<Preview />} />
-                      <Route path="paymenttype" element={<PaymentType />} />
-                      <Route path="userprofile" element={<UserProfile />} />
-                      <Route
-                        path="onlinepaymentconnector"
-                        element={<OnlinePaymentConnector />}
-                      />
-                      <Route path="*" element={<PageNotFound />} />
-                    </>
-                  ) : (
-                    <Route path="*" element={<PageNotFound />} />
-                  )}
-                </Route>
-              ) : (
-                // <Route path="*" element={<PageNotFound />} />
-                ""
-              )}
-              {/* ) :  <Route path="*" element={<PageNotFound />} />} */}
-            </Routes>
-          )}
-          {auth === "false" && (
-            <Routes>
-              <Route
-                exact
-                path="*"
-                element={
-                  keycloakData ? (
-                    <Navigate to="/home" />
-                  ) : (
-                    <Navigate to="/user-verification" />
-                  )
-                }
-              />
-              <Route
-                exact
-                path="/home"
-                element={
-                  <Home setIsLoggedIn={setIsLoggedIn} isLoggedIn={true} />
-                }
-              />
-              {/* <Route path="/" element={<Home isLoggedIn={true} />} /> */}
-              {/* <Route path="/home" element={<Navigate to={"/"} />} /> */}
-              <Route path="/user-verification" element={<PageNotFound />} />
-              <Route path="/signin" element={<Signin />} />
+              {/* <Route path="/user-verification" element={<PageNotFound />} /> */}
+              {/* <Route path="/signin" element={<Signin />} /> */}
               <Route path="/dashboard" element={<SidebarNew color="light" />}>
                 <Route path="" element={<Dashboard />} />
-                <Route path="language" element={<Language />} />
-                <Route
-                  path="language/edit_language"
-                  element={<EditLanguage />}
-                />
-                <Route path="language/add_language" element={<AddLanguage />} />
-                <Route path="store" element={<Store />} />
-                <Route path="store/storesetting" element={<StoreSettings />} />
-                <Route path="preview" element={<Preview />} />
-                <Route path="paymenttype" element={<PaymentType />} />
-                <Route path="userprofile" element={<UserProfile />} />
-                <Route
-                  path="onlinepaymentconnector"
-                  element={<OnlinePaymentConnector />}
-                />
-                {/* <Route path="*" element={<PageNotFound />} /> */}
+                {/* <Route
+                  path=""
+                  element={
+                    <Dashboard
+                      setIsLoggedIn={setIsLoggedIn}
+                      isLoggedIn={isLoggedIn}
+                    />
+                  }
+                /> */}
+                <>
+                  <Route path="language" element={<Language />} />
+                  <Route
+                    path="language/edit_language"
+                    element={<EditLanguage />}
+                  />
+                  <Route
+                    path="language/add_language"
+                    element={<AddLanguage />}
+                  />
+                  <Route path="store" element={<Store />} />
+                  <Route
+                    path="store/storesetting"
+                    element={<StoreSettings />}
+                  />
+                  <Route path="preview" element={<Preview />} />
+                  <Route path="paymenttype" element={<PaymentType />} />
+                  <Route path="userprofile" element={<UserProfile />} />
+                  <Route
+                    path="onlinepaymentconnector"
+                    element={<OnlinePaymentConnector />}
+                  />
+                  <Route path="*" element={<PageNotFound />} />
+                </>
               </Route>
-              {/* <Route path="*" element={<PageNotFound />} /> */}
+              {/* ) :  <Route path="*" element={<PageNotFound />} />} */}
             </Routes>
-          )}
-        </Container>
-        <Footer />
-      </Router>
-    </Suspense>
-  );
+          </Container>
+          <Footer />
+        </Router>
+      </Suspense>
+    );
+  }
+
+  return <>{void auth.signinRedirect()}</>;
+  // return (
+  //   <Suspense fallback={<LoadingMarkup />}>
+  //     <Router>
+  //       <ToastContainer />
+  //       <Header2 />
+  //       <Container fluid className="p-0 bg-[#F4F4F4] text-[#393939]">
+  //         {authFromEnv === "true" && (
+  //           <Routes>
+  //             <Route
+  //               exact
+  //               path="*"
+  //               element={
+  //                 keycloakData ? (
+  //                   <Navigate to="/home" />
+  //                 ) : (
+  //                   <Navigate to="/user-verification" />
+  //                 )
+  //               }
+  //             />
+  //             <Route
+  //               path="/"
+  //               element={
+  //                 <Home setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
+  //               }
+  //             />
+  //             {/* <Route path="/home" element={<Navigate to={"/"} />} /> */}
+  //             <Route path="/user-verification" element={<PageNotFound />} />
+  //             <Route path="/signin" element={<Signin />} />
+  //             {keycloakData ? (
+  //               <Route path="/dashboard" element={<SidebarNew color="light" />}>
+  //                 <Route
+  //                   path=""
+  //                   element={
+  //                     <Dashboard
+  //                       setIsLoggedIn={setIsLoggedIn}
+  //                       isLoggedIn={isLoggedIn}
+  //                     />
+  //                   }
+  //                 />
+  //                 {isLoggedInFromSession ? (
+  //                   <>
+  //                     <Route path="language" element={<Language />} />
+  //                     <Route
+  //                       path="language/edit_language"
+  //                       element={<EditLanguage />}
+  //                     />
+  //                     <Route
+  //                       path="language/add_language"
+  //                       element={<AddLanguage />}
+  //                     />
+  //                     <Route path="store" element={<Store />} />
+  //                     <Route
+  //                       path="store/storesetting"
+  //                       element={<StoreSettings />}
+  //                     />
+  //                     <Route path="preview" element={<Preview />} />
+  //                     <Route path="paymenttype" element={<PaymentType />} />
+  //                     <Route path="userprofile" element={<UserProfile />} />
+  //                     <Route
+  //                       path="onlinepaymentconnector"
+  //                       element={<OnlinePaymentConnector />}
+  //                     />
+  //                     <Route path="*" element={<PageNotFound />} />
+  //                   </>
+  //                 ) : (
+  //                   <Route path="*" element={<PageNotFound />} />
+  //                 )}
+  //               </Route>
+  //             ) : (
+  //               // <Route path="*" element={<PageNotFound />} />
+  //               ""
+  //             )}
+  //             {/* ) :  <Route path="*" element={<PageNotFound />} />} */}
+  //           </Routes>
+  //         )}
+  //         {authFromEnv === "false" && (
+  //           <Routes>
+  //             <Route
+  //               exact
+  //               path="*"
+  //               element={
+  //                 keycloakData ? (
+  //                   <Navigate to="/home" />
+  //                 ) : (
+  //                   <Navigate to="/user-verification" />
+  //                 )
+  //               }
+  //             />
+  //             <Route
+  //               exact
+  //               path="/home"
+  //               element={
+  //                 <Home setIsLoggedIn={setIsLoggedIn} isLoggedIn={true} />
+  //               }
+  //             />
+  //             {/* <Route path="/" element={<Home isLoggedIn={true} />} /> */}
+  //             {/* <Route path="/home" element={<Navigate to={"/"} />} /> */}
+  //             <Route path="/user-verification" element={<PageNotFound />} />
+  //             <Route path="/signin" element={<Signin />} />
+  //             <Route path="/dashboard" element={<SidebarNew color="light" />}>
+  //               <Route path="" element={<Dashboard />} />
+  //               <Route path="language" element={<Language />} />
+  //               <Route
+  //                 path="language/edit_language"
+  //                 element={<EditLanguage />}
+  //               />
+  //               <Route path="language/add_language" element={<AddLanguage />} />
+  //               <Route path="store" element={<Store />} />
+  //               <Route path="store/storesetting" element={<StoreSettings />} />
+  //               <Route path="preview" element={<Preview />} />
+  //               <Route path="paymenttype" element={<PaymentType />} />
+  //               <Route path="userprofile" element={<UserProfile />} />
+  //               <Route
+  //                 path="onlinepaymentconnector"
+  //                 element={<OnlinePaymentConnector />}
+  //               />
+  //               {/* <Route path="*" element={<PageNotFound />} /> */}
+  //             </Route>
+  //             {/* <Route path="*" element={<PageNotFound />} /> */}
+  //           </Routes>
+  //         )}
+  //       </Container>
+  //       <Footer />
+  //     </Router>
+  //   </Suspense>
+  // );
 };
 
 export default App;
--- src/services/axios/SetupInterceptors.js
+++ src/services/axios/SetupInterceptors.js
@@ -27,38 +27,38 @@ export const SetupInterceptors = (http) => {
     },
     function (error) {
       // debugger;
-      const originalConfig = error.config;
-      if (error.response) {
-        if (error.response.status === 401 && !originalConfig._retry) {
-          originalConfig._retry = true;
+      // const originalConfig = error.config;
+      // if (error.response) {
+      //   if (error.response.status === 401 && !originalConfig._retry) {
+      //     originalConfig._retry = true;
 
-          // Get refreshToken
-          let refreshToken = util.getRefreshToken();
-          let accessToken = util.getAuthToken();
-          let accessPoint = umsBaseURL + refreshTokenAPI;
-          MarketplaceServices.save(accessPoint, {
-            refresh_token: refreshToken,
-            access_token: accessToken,
-          })
-            .then((response) => {
-              console.log("retrieveRefreshToken", response.data);
-              if (response.data.access_token) {
-                util.setAuthToken(response.data.access_token);
-                util.setRefreshToken(response.data.refresh_token);
-                window.location.reload();
-              }
-            })
-            .catch((error) => {
-              console.log("Unable to get refresh token: ", error);
-              util.logoutUser();
-            });
-          return Promise.reject(error);
-        }
-        if (error.response.status === "ANOTHER_STATUS_CODE") {
-          // Do something
-          return Promise.reject(error.response.data);
-        }
-      }
+      //     // Get refreshToken
+      //     let refreshToken = util.getRefreshToken();
+      //     let accessToken = util.getAuthToken();
+      //     let accessPoint = umsBaseURL + refreshTokenAPI;
+      //     MarketplaceServices.save(accessPoint, {
+      //       refresh_token: refreshToken,
+      //       access_token: accessToken,
+      //     })
+      //       .then((response) => {
+      //         console.log("retrieveRefreshToken", response.data);
+      //         if (response.data.access_token) {
+      //           util.setAuthToken(response.data.access_token);
+      //           util.setRefreshToken(response.data.refresh_token);
+      //           window.location.reload();
+      //         }
+      //       })
+      //       .catch((error) => {
+      //         console.log("Unable to get refresh token: ", error);
+      //         util.logoutUser();
+      //       });
+      //     return Promise.reject(error);
+      //   }
+      //   if (error.response.status === "ANOTHER_STATUS_CODE") {
+      //     // Do something
+      //     return Promise.reject(error.response.data);
+      //   }
+      // }
       // console.log("SetupInterceptors-response-error", error);
       // const status = error?.response?.status || 0;
       // const resBaseURL = error?.response?.config?.baseURL;
