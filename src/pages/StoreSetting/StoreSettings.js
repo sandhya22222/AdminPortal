bff4275e9e79b1614fe9619f62c92c72321da632
Sandhya Allu <sandhya_allu@thbs.india.com>
2023-06-24 20:08:27 +0530

    fixed image related issue

--- src/pages/StoreSetting/StoreSettings.js
+++ src/pages/StoreSetting/StoreSettings.js
@@ -1067,8 +1067,8 @@ const StoreSettings = () => {
         }
         // setGetImageData([response.data]);
         // !TODO: Update response is not , backend is giving null for previously updated images So we are doing get call here again.
-        findAllWithoutPageStoreImagesApi(id);
-        window.location.reload();
+        // findAllWithoutPageStoreImagesApi(id);
+        // window.location.reload();
         setIsLoading(false);
         console.log(
           "Server Success Response From storeImagePutCall",
