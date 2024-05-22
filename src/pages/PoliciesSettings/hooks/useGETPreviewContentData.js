74444af71670126fa5d16a46152e7cb1d8d2a0d2
venugopal <venugopal_murugan@thbs.com>
2024-05-22 13:57:11 +0530

    Following issue has been fixed in AP MP-3031, MP-3027

---
+++ src/pages/PoliciesSettings/hooks/useGETPreviewContentData.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+import MarketplaceServices from '../../../services/axios/MarketplaceServices'
+const BASE_URL = process.env.REACT_APP_GET_USER_CONSENT_NEW
+
+const useGetPreviewContentData = ({ storeName }) => {
+    const getPreviewContent = async () => {
+        const params = {
+            'store-name': storeName,
+        }
+        const res = await MarketplaceServices.findAll(BASE_URL, params)
+        return res?.data?.response_body
+    }
+    return useQuery({
+        queryKey: ['consent_preview', storeName],
+        queryFn: getPreviewContent,
+    })
+}
+export default useGetPreviewContentData
