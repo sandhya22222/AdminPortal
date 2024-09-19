c1b9c1e068b8ac2008a64348eea3e2254a59d416
Chris <chris_austin@thbs.com>
2024-09-19 16:45:34 +0530

    Migrated Store listing page to shadcn

--- src/pages/Stores/Store.js
+++ src/pages/Stores/Store.js
@@ -15,12 +15,13 @@ import {
     Progress,
     InputNumber,
     Alert,
-    Badge,
+    // Badge,
     Empty,
     Segmented,
     Tag,
 } from 'antd'
 import React, { useEffect, useState } from 'react'
+
 import validator from 'validator'
 import { MdInfo } from 'react-icons/md'
 import { Link, useLocation, useSearchParams, useNavigate } from 'react-router-dom'
@@ -33,8 +34,8 @@ import {
     warningInfoIcon,
 } from '../../constants/media'
 //! Import user defined components
-import DynamicTable from '../../components/DynamicTable/DynamicTable'
-import DynamicTable2 from '../../components/DynamicTable/DynamicTable2'
+// import DynamicTable from '../../components/DynamicTable/DynamicTable'
+// import DynamicTable2 from '../../components/DynamicTable/DynamicTable2'
 import HeaderForTitle from '../../components/header/HeaderForTitle'
 import StoreModal from '../../components/storeModal/StoreModal'
 import { usePageTitle } from '../../hooks/usePageTitle'
@@ -42,15 +43,17 @@ import MarketplaceServices from '../../services/axios/MarketplaceServices'
 import Status from './Status'
 import MarketplaceToaster from '../../util/marketplaceToaster'
 import util from '../../util/common'
-import StoreContent from './StoreContent'
-import Store2 from './Store2'
-import StoreTable from './StoreTable'
+
 import { Toggle } from '../../shadcnComponents/ui/toggle'
 import DmPagination2 from '../../components/DmPagination/DmPagination2'
 import axios from 'axios'
 import { useAuth } from 'react-oidc-context'
 import { validatePositiveNumber } from '../../util/validation'
-import ShadCNPagination from '../../components/shadCNCustomComponents/ShadCNPagination'
+
+import ShadCNTable from '../../components/shadCNCustomComponents/ShadCNTable'
+import StoreListing from './StoreListing'
+import { Star } from 'lucide-react'
+import { Badge } from '../../shadcnComponents/ui/badge'
 
 const { Content } = Layout
 const { Title, Text } = Typography
@@ -418,11 +421,19 @@ const Stores = () => {
                                 </Row>
                                 <Row>
                                     {record.isDistributor ? (
-                                        <Tag color='blue'>
-                                            <StarFilled /> {t('labels:distributor')}
-                                        </Tag>
+                                        <Badge className='bg-[#E6F4FF] border-[#91CAFF] text-[#0958D9] flex items-center'>
+                                            <Star
+                                                className='w-3 h-3 mr-1'
+                                                fill='#0958D9' // Fill color
+                                                // stroke='#0958D9'Stroke color (if needed)
+                                                strokeWidth={0} // Set stroke width to 0 to hide the outline
+                                            />
+                                            {t('labels:distributor')}
+                                        </Badge>
                                     ) : (
-                                        <Tag color='cyan'>{t('labels:partner')}</Tag>
+                                        <Badge className='bg-[#E6FFFB] border-[#87E8DE] text-[#08979C]'>
+                                            {t('labels:partner')}
+                                        </Badge>
                                     )}{' '}
                                     {!isDistributor && (
                                         <Tooltip title={t('messages:store_type_info')}>
@@ -1347,23 +1358,29 @@ const Stores = () => {
                                                 {t('labels:my_stores')}
                                             </div>
                                             <div className='flex items-center justify-end flex-row gap-3 flex-grow'>
-                                                <div className='flex flex-row '>
+                                                <div className='flex flex-row'>
                                                     <Toggle
-                                                        className='rounded-l-[7px] '
+                                                        className='rounded-l-[7px]'
+                                                        variant={value === 0 ? 'active' : 'default'}
                                                         checked={value === 0}
                                                         onClick={() => handleToggleChange(0)}>
                                                         {t('labels:all')}
                                                     </Toggle>
-                                                    <Toggle checked={value === 1} onClick={() => handleToggleChange(1)}>
+                                                    <Toggle
+                                                        variant={value === 1 ? 'active' : 'default'}
+                                                        checked={value === 1}
+                                                        onClick={() => handleToggleChange(1)}>
                                                         {t('labels:active')}
                                                     </Toggle>
                                                     <Toggle
                                                         className='rounded-r-[7px]'
+                                                        variant={value === 2 ? 'active' : 'default'}
                                                         checked={value === 2}
                                                         onClick={() => handleToggleChange(2)}>
                                                         {t('labels:inactive')}
                                                     </Toggle>
                                                 </div>
+
                                                 <Search
                                                     placeholder={t('placeholders:please_enter_search_text_here')}
                                                     onSearch={handleSearchChange}
@@ -1429,33 +1446,23 @@ const Stores = () => {
                                                                     />
                                                                 </div>
                                                             )}
-                                                        <StoreTable storeTableData={storeTableData} t={t} />
-                                                        {/* <DynamicTable tableComponentData={storeTableData} /> */}
+                                                        {/* <StoreTable storeTableData={storeTableData} t={t} /> */}
+                                                        {/* <DynamicTable2 tableComponentData={storeTableData} /> */}
+                                                        <StoreListing tableComponentData={storeTableData} />
 
                                                         {parseInt(m_tab_id) === 1 ? (
                                                             <Content className=' grid justify-items-end mx-3 h-fit'>
                                                                 {countForStore && countForStore >= pageLimit ? (
                                                                     <DmPagination2
                                                                         currentPage={
-                                                                            parseInt(searchParams.get('page'))
-                                                                                ? parseInt(searchParams.get('page'))
-                                                                                : 1
-                                                                        }
-                                                                        presentPage={
-                                                                            parseInt(searchParams.get('page'))
-                                                                                ? parseInt(searchParams.get('page'))
-                                                                                : 1
+                                                                            parseInt(searchParams.get('page')) || 1
                                                                         }
                                                                         totalItemsCount={countForStore}
-                                                                        defaultPageSize={pageLimit}
-                                                                        pageSize={
-                                                                            parseInt(searchParams.get('limit'))
-                                                                                ? parseInt(searchParams.get('limit'))
-                                                                                : pageLimit
+                                                                        itemsPerPage={
+                                                                            parseInt(searchParams.get('limit')) ||
+                                                                            pageLimit
                                                                         }
                                                                         handlePageNumberChange={handlePageNumberChange}
-                                                                        showSizeChanger={true}
-                                                                        showTotal={true}
                                                                         showQuickJumper={true}
                                                                     />
                                                                 ) : null}
